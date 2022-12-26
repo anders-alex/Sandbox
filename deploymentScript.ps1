e2ed0c957b54f5b5640d7c3d353b1e8cfd02790e
Alex Anders <50784041+anders-alex@users.noreply.github.com>
2022-12-26 13:02:31 -0700

    Add files via upload

--- deploymentScript.ps1
+++ deploymentScript.ps1
@@ -1,4 +1,4 @@
-param([string] $PackageUri, [string] $SubscriptionId, [string] $ResourceGroupName, [string] $FunctionAppName, [string] $KeyVaultName, [string] $FAScope, [string] $KVScope, [string] $UAMIPrincipalId)
+param([string] $PackageUri, [string] $SubscriptionId, [string] $ResourceGroupName, [string] $FunctionAppName, [string] $FAScope, $UAMIPrincipalId)
 
 Set-AzContext -Subscription $SubscriptionId
 
@@ -6,15 +6,6 @@ Set-AzContext -Subscription $SubscriptionId
 Invoke-WebRequest -Uri $PackageUri -OutFile functionPackage.zip
 Publish-AzWebapp -ResourceGroupName $ResourceGroupName -Name $FunctionAppName -ArchivePath functionPackage.zip -Force
 
-#Get list of Function App outbound IPs so we can restrict network access on the Key Vault.
-$functionAppIps = (Get-AzFunctionApp -ResourceGroupName $ResourceGroupName -Name $FunctionAppName).OutboundIPAddress.Split(',')
-
-foreach ($ip in $functionAppIps) {
-    Add-AzKeyVaultNetworkRule -VaultName $KeyVaultName -IpAddressRange $ip
-}
-Update-AzKeyVaultNetworkRuleSet -VaultName $KeyVaultName -DefaultAction Deny -Bypass None
-
 #Cleanup the Service Principal Owner role assignments now that access is no longer needed.
 Remove-AzRoleAssignment -ObjectId $UAMIPrincipalId -RoleDefinitionName Owner -Scope $FAScope
-Remove-AzRoleAssignment -ObjectId $UAMIPrincipalId -RoleDefinitionName Owner -Scope $KVScope
   
